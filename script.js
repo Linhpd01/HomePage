e1eaaa6fe1585734404ebca8e5272a10244b487d
Duy Linh Pham <99628680+Linhpd01@users.noreply.github.com>
2022-04-25 00:56:01 +0700

    Website HomePage
    Create Website HomePage

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.product').slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows:true,
+    prevArrow: false,
+    nextArrow: false,
+  });
